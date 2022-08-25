fe60be794a2d2939b93efd7a75798022f03ffbb9
Emerson Teixeira <emersonrosateixeira@hotmail.com>
2022-08-25 19:55:07 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'doris-front';
+export class AppComponent implements OnInit, OnDestroy {
+
+    dataFromDorisEvent: Subscription = new Subscription();
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.dataFromDorisEvent = fromEvent(window, 'sendDataToAngular').subscribe((result: any) => {
+            console.log('variaveis_salvas no app:', result?.detail);
+        });
+    }
+
+    ngOnDestroy() {
+        this.dataFromDorisEvent.unsubscribe();
+    }
+
 }
